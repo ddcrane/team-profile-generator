65b7e4eb4faf30c905733b96e1730cceaffd7d25
David Crane <ddcrane2@gmail.com>
2021-10-13 17:48:33 -0500

    distribution

--- index.js
+++ index.js
@@ -6,68 +6,77 @@ const inquirer = require('inquirer');
 const path = require("path");
 const fs = require("fs");
 
-let employeeData = [];
+const OUTPUT_DIR = path.resolve(__dirname, "dist")
+const outputPath = path.join(OUTPUT_DIR, "index.html");
+
+const render = require("./src/template.js");
+
+const teamMembers = [];
+const idArray = [];
 
 const promptUser = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            name: 'name',
+            name: 'managerName',
             message: "What is your Team Manager's name?"
         },
         {
             type: 'input',
-            name: 'id',
+            name: 'managerId',
             message: 'Enter your employee ID.',
         },
         {
             type: 'input',
-            name: 'email',
+            name: 'managerEmail',
             message: 'Enter your email address.',
         },
         {
             type: 'input',
-            name: 'officeNumber',
+            name: 'managerOfficeNumber',
             message: 'Enter your office number.',
         },
-        {
-            type: 'rawlist',
-            name: 'type',
-            message: 'Select the new employee position.',
-            choices: ['Engineer', 'Intern', 'Manager'],
-            
-        },
-    ])
 
+    ])
+    .then(answers => {
+        const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+        teamMembers.push(manager);
+        idArray.push(answers.managerId);
+        createTeam();
+      });
 };
 
 
 
 const managerQuestions = () => {
     return inquirer.prompt([
-    {
-        type: 'input',
-        name: 'name',
-        message: "What is your Team Manager's name?"
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'Enter your employee ID.',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email address.',
-    },
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'Enter your office number.',
-    },
+        {
+            type: 'input',
+            name: 'managerName',
+            message: "What is your Team Manager's name?"
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: 'Enter your employee ID.',
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: 'Enter your email address.',
+        },
+        {
+            type: 'input',
+            name: 'managerOfficeNumber',
+            message: 'Enter your office number.',
+        },
 ])
-
-
+.then(answers => {
+    const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+    teamMembers.push(manager);
+    idArray.push(answers.managerId);
+    createTeam();
+  });
 };
 
 
@@ -75,26 +84,32 @@ const engineerQuestions = () => {
     return inquirer.prompt([
         {
            type: 'input',
-           name: 'name',
+           name: 'engineerName',
            message: "What is your engineer's name?"
        },
        {
            type: 'input',
-           name: 'id',
+           name: 'engineerId',
            message: 'Enter their employee ID.',
        },
        {
            type: 'input',
-           name: 'email',
+           name: 'engineerEmail',
            message: 'Enter their email address.',
        },
        {
         type: 'input',
-        name: 'github',
+        name: 'engineerGithub',
         message: 'Enter their GitHub username.',
         },
 
     ])
+    .then(answers => {
+        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+        teamMembers.push(engineer);
+        idArray.push(answers.engineerId);
+        createTeam();
+      });
 };
 
 
@@ -102,46 +117,83 @@ const internQuestions = () => {
     return inquirer.prompt([
         {
            type: 'input',
-           name: 'name',
+           name: 'internName',
            message: "What is your intern's name?"
        },
        {
            type: 'input',
-           name: 'id',
+           name: 'internId',
            message: 'Enter their employee ID.',
        },
        {
            type: 'input',
-           name: 'email',
+           name: 'internEmail',
            message: 'Enter their email address.',
        },
        {
         type: 'input',
-        name: 'school',
+        name: 'internSchool',
         message: 'Enter their school name.',
         },
 
     ])
+    .then(answers => {
+        const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+        teamMembers.push(intern);
+        idArray.push(answers.internId);
+        createTeam();
+      });
 };
 
 
+function buildTeam() {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+  }
 
+  function createTeam() {
 
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "memberChoice",
+        message: "Which type of team member would you like to add?",
+        choices: [
+          "Engineer",
+          "Intern",
+          "I don't want to add any more team members"
+        ]
+      }
+    ]).then(userChoice => {
+      switch (userChoice.memberChoice) {
+        case "Engineer":
+            engineerQuestions();
+          break;
+        case "Intern":
+            internQuestions();
+          break;
+        default:
+          buildTeam();
+      }
+    });
+  }
 
 promptUser()
-.then(prompts => {
-    console.log(prompts);
+// .then(prompts => {
+//     console.log(prompts);
     
-    if (prompts.type === 'Engineer') {
-        return engineerQuestions();
-    } 
-    if (prompts.type === 'Intern') {
-        return internQuestions();
-    } 
-    if (prompts.type === 'Manager') {
-        return managerQuestions();
-    } 
-})
+//     if (prompts.type === 'Engineer') {
+//         return engineerQuestions();
+//     } 
+//     if (prompts.type === 'Intern') {
+//         return internQuestions();
+//     } 
+//     if (prompts.type === 'Manager') {
+//         return managerQuestions();
+//     } 
+// })
 
 
 
